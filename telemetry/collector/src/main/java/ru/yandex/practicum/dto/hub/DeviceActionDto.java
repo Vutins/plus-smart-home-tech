8b1a67264c526b98b0e137b106a5f28d47cb1103
Nikulshina Polina <nikulschina-p@yandex.ru>
2026-04-27 09:28:38 +0400

    add dto hub

---
+++ telemetry/collector/src/main/java/ru/yandex/practicum/dto/hub/DeviceActionDto.java
@@ -0,0 +1,16 @@
+package ru.yandex.practicum.dto.hub;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import ru.yandex.practicum.kafka.telemetry.event.ActionTypeAvro;
+
+@Getter
+@Setter
+@ToString
+public class DeviceActionDto {
+
+    private String sensorId;
+    private ActionTypeAvro type;
+    private Integer value;
+}
---
+++ telemetry/collector/src/main/java/ru/yandex/practicum/dto/hub/ScenarioConditionDto.java
@@ -0,0 +1,18 @@
+package ru.yandex.practicum.dto.hub;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import ru.yandex.practicum.kafka.telemetry.event.ConditionOperationAvro;
+import ru.yandex.practicum.kafka.telemetry.event.ConditionTypeAvro;
+
+@Getter
+@Setter
+@ToString
+public class ScenarioConditionDto<T> {
+
+    private String sensorId;
+    private ConditionTypeAvro type;
+    private ConditionOperationAvro operation;
+    private T value;
+}
